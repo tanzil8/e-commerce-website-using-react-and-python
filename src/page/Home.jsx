d6afcfc5bac4df29a6966c2e6e14f1c66f5ee308
tanzil8 <khantanzil383@gmail.com>
2025-09-11 12:09:19 -0700

    Shadcn ui install and setup

---
+++ src/page/Home.jsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+
+
+
+function Home() {
+ 
+
+  return (
+    <>
+   <h1>Home</h1>
+   <Button>hello</Button>
+    </>
+  )
+}
+
+export default Home
