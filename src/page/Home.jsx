ca3a9e53e19defa4a77cae5a153fe4d71f421d5f
tanzil8 <khantanzil383@gmail.com>
2026-01-23 10:52:07 -0800

    react setup

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,27 +1,9 @@
-import { Header } from "../component/HomePage/header"
-import { Banner } from "../component/HomePage/banner"
-import { HeroSection } from "../component/HomePage/hero-section"
-import { FlashSale } from "../component/HomePage/flash-sale"
-import { Categories } from "../component/HomePage/categories"
-import { BestSelling } from "../component/HomePage/best-selling"
-import { FeaturedProducts } from "../component/HomePage/featured-products"
-import { Newsletter } from "../component/HomePage/newsletter"
-import { Footer } from "../component/HomePage/footer"
+
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background">
-      <Banner />
-      <Header />
-      <main>
-        <HeroSection />
-        <FlashSale />
-        <Categories />
-        <BestSelling />
-        <FeaturedProducts />
-        <Newsletter />
-      </main>
-      <Footer />
+      <h1>tanzil</h1>
     </div>
   )
 }
