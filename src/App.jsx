f5bea41a7019883a064ae705bb9c0e87d42abd3a
tanzil8 <khantanzil383@gmail.com>
2026-02-04 10:35:13 -0800

    50% UI complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={} />
       </Routes>
     </BrowserRouter>
   );
